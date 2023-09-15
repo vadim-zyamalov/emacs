3e12854bc32e09a3eed375277268bd949dfc71fb
Vadim Zyamalov <zyamalov.ve@yandex.ru>
2023-09-15 23:06:43 +0300

    Настройки полностью перелопачены
    1. Удалены старые настройки, использующие `setup.el`.
    2. Теперь org-файл с настроками теперь не загружается, а преобразуется в
       `el` при сохранении.

--- early-init.el
+++ early-init.el
@@ -46,17 +46,4 @@
 (setq native-comp-speed -1)
 (setq straight-check-for-modifications '(check-on-save find-when-checking))
 
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 6))
-    (unless (file-exists-p bootstrap-file)
-        (with-current-buffer
-                (url-retrieve-synchronously
-                 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-                 'silent 'inhibit-cookies)
-            (goto-char (point-max))
-            (eval-print-last-sexp)))
-    (load bootstrap-file nil 'nomessage))
-
 ;;; early-init.el ends here
